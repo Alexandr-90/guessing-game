284e5aa7dd8ec56aef2f10a5503c322717a1ed89
Aleksandr Tarasov <tarasov.alexander.v@gmail.com>
2019-10-14 20:06:19 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.min + this.max)/2); // we divide the number into two parts and look in the middle for solution...
+        return this.result; //..candidate then we ask if solution in one part or in another then repeat again with a half range
     }
 
     lower() {
-
+        this.max = this.result;
+        return this.max;
     }
 
     greater() {
-
+        this.min = this.result;
+        return this.min;
     }
 }
 
